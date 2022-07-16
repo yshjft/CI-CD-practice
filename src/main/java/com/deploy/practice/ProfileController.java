3c8aa6f32d47ddf73b1fd5290f5b11ed5ef2bd43
hong yuseok <jericoh@naver.com>
2022-07-17 01:21:29 +0900

    feat : profile을 알려주는 api 추가

---
+++ src/main/java/com/deploy/practice/ProfileController.java
@@ -0,0 +1,18 @@
+package com.deploy.practice;
+
+import java.util.Arrays;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ProfileController {
+  private final Environment environment;
+
+  @GetMapping("/profile")
+  public String getProfile() {
+    return Arrays.stream(environment.getActiveProfiles()).findFirst().orElse("");
+  }
+}
